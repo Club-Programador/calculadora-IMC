26d1aa47a0203385176a24bb2bf6a3be9a2cb60a
Sebastian Iglesias <sebaezeq@gmail.com>
2023-04-22 11:08:31 -0300

    Estructura basica

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = iniciar;
+
+function iniciar() {
+   var btnCalcular=  document.getElementById("btnCalcular");
+   btnCalcular.addEventListener("click",clickBtnCalcular);
+}
+
+function clickBtnCalcular() {
+    var txtPeso=  document.getElementById("txtPeso");
+    var peso =txtPeso.value;
+    var txtAltura=  document.getElementById("txtAltura");
+    var altura= txtAltura.value;
+
+    var imc=peso/(altura*altura)
+
+    document.write("Su Indice de Masa Corporal es: "+ imc)
+    document.write("<br>")
+    document.write("<br>")
+    document.write("<br>")
+    document.write("<br>")
+    document.write('<p style="img-aling:center"><img src="https://www.doctorjosevicenteferrer.com/wp-content/uploads/2013/10/tabla-IMC-624x411.png" alt=""></p')
+    
+
+    
+
+
+}
