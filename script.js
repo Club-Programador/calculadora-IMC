e86c5936544680cbb21bdcfc36beaf521d6802f1
BMateo <75537669+NarMaroth@users.noreply.github.com>
2023-05-13 10:42:22 -0300

    fix & Changes + Favicon

--- script.js
+++ script.js
@@ -1,56 +1,43 @@
-iniciar()
 
-const wInput = document.getElementById("txtPeso");
-const wRange = document.getElementById("range-Peso");
-const hInput = document.getElementById("txtAltura");
-const hRange = document.getElementById("range-Altura");
-const p = document.querySelector("#resultOp");
+const PESO_TEXT = document.getElementById("input-peso-text");
+const PESO_RANGE = document.getElementById("input-peso-range");
+const ALTURA_TEXT = document.getElementById("input-altura-text");
+const ALTURA_RANGE = document.getElementById("input-altura-range");
+const RESULTADO = document.getElementById("resultado");
 
-wRange.addEventListener("input", () => {
-  wInput.value = wRange.value;
-});
-wInput.addEventListener("input", () => {
-  wRange.value = wInput.value;
-});
-hRange.addEventListener("input", () => {
-  hInput.value = hRange.value;
-});
-hInput.addEventListener("input", () => {
-  hRange.value = hInput.value;
-});
-
-function operacion() {
+function displayResultado() {
   // Cálculo del IMC
-  var txtPeso = document.getElementById("txtPeso");
-  var peso = parseFloat(txtPeso.value);
-  var txtAltura = document.getElementById("txtAltura");
-  var altura = parseFloat(txtAltura.value);
+  var peso = parseFloat(PESO_TEXT.value);
+  var altura = parseFloat(ALTURA_TEXT.value);
   var imc = peso / (altura * altura);
-  p.innerText = "Su Indice de Masa Corporal es: " + imc.toFixed(2);
+  RESULTADO.innerText = `\n${imc.toFixed(2)}`;
 
-  // Mostrar la imagen
-  var imgMostrar = document.getElementById("imgMostrar");
-  imgMostrar.src =
-    "https://www.doctorjosevicenteferrer.com/wp-content/uploads/2013/10/tabla-IMC-624x411.png";
-
-  // Mostrar el div que contiene la image
-  var divImagen = document.getElementById("imagen");
-  divImagen.style.display = "block";
+  document.querySelector(".resultado-imc").classList.toggle("hidden")
+  document.querySelector(".calculadora-imc").classList.toggle("hidden")
 }
 
-function iniciar() {
-  // Ocultar la imagen al cargar la página
-  var divImagen = document.getElementById("imagen");
-  divImagen.style.display = "none";
-
-
-
-  // Asignar el evento click al botón de calcular
-  var btnCalcular = document.querySelector("#btnCalcular");
-  btnCalcular.type = "button"; // Cambiar el tipo del botón
-  btnCalcular.addEventListener("click", function (event) {
-    event.preventDefault(); // Cancelar la acción predeterminada
-    operacion();
-  });
+function onClickResultado() {
+  if (PESO_TEXT.value !== "" && ALTURA_TEXT.value !== "") {
+    displayResultado()
+  }
 }
+
+
+PESO_RANGE.addEventListener("input", () => {
+  PESO_TEXT.value = PESO_RANGE.value;
+});
+PESO_TEXT.addEventListener("input", () => {
+  PESO_RANGE.value = PESO_TEXT.value;
+});
+ALTURA_RANGE.addEventListener("input", () => {
+  ALTURA_TEXT.value = ALTURA_RANGE.value;
+});
+ALTURA_TEXT.addEventListener("input", () => {
+  ALTURA_RANGE.value = ALTURA_TEXT.value;
+});
+
+// Asignar el evento click al botón de calcular
+document.getElementById("btn-calcular").addEventListener("click", function (event) {
+  onClickResultado();
+});
 
